ffeffcbc048e11883fafaca983104805fb0db1db
peroks <per.egil@roksvaag.com>
2023-06-09 23:32:39 +0200

    Rewrite Model::PrepareProperty

--- src/Model.php
+++ src/Model.php
@@ -1,5 +1,11 @@
 <?php namespace Peroks\Model\Store;
 
+use Peroks\Model\Property;
+use Peroks\Model\PropertyItem;
+use Peroks\Model\PropertyType;
+use Peroks\Model\Utils;
+use Traversable;
+
 /**
  * The store model class.
  *
@@ -16,5 +22,37 @@ class Model extends \Peroks\Model\Model implements ModelInterface {
 	 */
 	public static function name(): string {
 		return static::class;
+	}
+
+	/**
+	 * Prepare a property value before inserting it into the model.
+	 *
+	 * @param mixed $value The property value.
+	 * @param Property|array $property The property definition.
+	 *
+	 * @return mixed The prepared property value.
+	 */
+	protected static function prepareProperty( $value, $property ) {
+		$value = parent::prepareProperty( $value, $property );
+		$type  = $property[ PropertyItem::TYPE ] ?? null;
+
+		if ( PropertyType::INTEGER === $type ) {
+			if ( is_string( $value ) && is_numeric( $value ) ) {
+				return (int) $value;
+			}
+		} elseif ( PropertyType::FLOAT === $type ) {
+			if ( is_string( $value ) && is_numeric( $value ) ) {
+				return (float) $value;
+			}
+		} elseif ( PropertyType::BOOL === $type ) {
+			if ( is_string( $value ) && is_numeric( $value ) ) {
+				$value = (int) $value;
+			}
+			if ( is_int( $value ) ) {
+				return (bool) $value;
+			}
+		}
+
+		return $value;
 	}
 }
